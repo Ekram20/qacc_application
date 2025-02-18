ef19f5344d24def1f9e1f77d5de90a85559cdead
Ekram20 <90451076+Ekram20@users.noreply.github.com>
2025-02-19 01:03:49 +0200

    الوضع الافقي

--- lib/widgets/image_text_card.dart
+++ lib/widgets/image_text_card.dart
@@ -15,53 +15,58 @@ class ImageTextCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.circular(10),
-        // border: Border.all(
-        //   color: AppColors.secondaryColor.shade400, // لون الحدود
-        //   width: 1.0, // سماكة الحدود
-        // ),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.2), // لون الظل مع شفافية
-            spreadRadius: 2, // انتشار الظل
-            blurRadius: 8, // درجة تمويه الظل
-            offset: Offset(4, 4), // إزاحة الظل (أفقيًا وعموديًا)
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 8,
+            offset: const Offset(4, 4),
           ),
           BoxShadow(
-            color: Colors.white.withOpacity(0.1), // لون ظل معاكس لعمق أكبر
-            spreadRadius: -2, // انتشار عكسي
+            color: Colors.white.withOpacity(0.1),
+            spreadRadius: -2,
             blurRadius: 8,
-            offset: Offset(-4, -4),
+            offset: const Offset(-4, -4),
           ),
         ],
       ),
+      padding: const EdgeInsets.all(8),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, // توسيط المحتوى داخل الكرت
-        mainAxisSize: MainAxisSize.min, // يجعل الارتفاع يعتمد على المحتوى
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ShaderMask(
-            shaderCallback: (Rect bounds) {
-              return LinearGradient(
-                colors: [
-                  AppColors.secondaryColor.shade600,
-                  AppColors.primaryColor.shade400,
-                ], // الألوان المتدرجة
-                begin: Alignment.topLeft, // بداية التدرج
-                end: Alignment.bottomRight, // نهاية التدرج
-              ).createShader(bounds);
-            },
-            blendMode: BlendMode.srcATop, // دمج اللون مع الصورة
-            child: Image.asset(
-              image, // مسار الصورة
-              width: 50, // عرض الصورة (اختياري)
-              height: 50, // ارتفاع الصورة (اختياري)
-              fit: BoxFit.cover, // كيفية ملاءمة الصورة (اختياري)
+          Flexible(
+            flex: 2,
+            child: ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return LinearGradient(
+                  colors: [
+                    AppColors.secondaryColor.shade600,
+                    AppColors.primaryColor.shade400,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ).createShader(bounds);
+              },
+              blendMode: BlendMode.srcATop,
+              child: Image.asset(
+                image,
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Gap(20),
-          Text(
-            mainText,
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
+          const Gap(10),
+          Flexible(
+            flex: 3,
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                mainText,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
         ],
       ),
