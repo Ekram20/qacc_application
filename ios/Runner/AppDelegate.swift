e960682ace923c92d5df66552c39be25ca5a131d
Rahma Abubaker Said <itsrahmasaid.official@gmail.com>
2025-06-17 09:53:01 +0200

    edit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FirebaseCore  // ✅ استيراد FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+        // ✅ تهيئة Firebase
+    FirebaseApp.configure()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
