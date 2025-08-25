4663256b2d9f5f0db6211262c506782721b943a9
Rahma Abubaker Said <itsrahmasaid.official@gmail.com>
2025-08-25 11:45:22 +0200

    fix(android): update Gradle and Android configs for successful run

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,8 @@ android {
         applicationId = "com.qacc.qacc_application"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        //  
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
